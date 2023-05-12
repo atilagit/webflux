7eb28396dd31b2973d1d5f4bcd52a37dc198412a
Atila Silva <atila__7@hotmail.com>
2023-05-12 19:13:47 -0300

    Alterações dos tipos de retorno

--- src/main/java/com/example/webflux/controllers/Controller.java
+++ src/main/java/com/example/webflux/controllers/Controller.java
@@ -1,6 +1,6 @@
 package com.example.webflux.controllers;
 
-import com.example.webflux.entities.Estados;
+import com.example.webflux.entities.EstadosComInfoCliente;
 import com.example.webflux.services.EstadosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +14,8 @@ public class Controller {
     EstadosService estadosService;
 
     @GetMapping(value = "/estados")
-    public ResponseEntity<Estados> getEstados() {
-        Estados estados = estadosService.getEstados();
+    public ResponseEntity<EstadosComInfoCliente> getEstados() {
+        EstadosComInfoCliente estados = estadosService.getEstados();
         return ResponseEntity.ok(estados);
     }
 }
--- src/main/java/com/example/webflux/services/EstadosService.java
+++ src/main/java/com/example/webflux/services/EstadosService.java
@@ -1,6 +1,6 @@
 package com.example.webflux.services;
 
-import com.example.webflux.entities.Estados;
+import com.example.webflux.entities.EstadosComInfoCliente;
 import com.example.webflux.repositories.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,9 +12,7 @@ public class EstadosService {
     @Autowired
     Repository repository;
 
-    public Estados getEstados() {
-        var estadosComInfoCliente = repository.getEstadosComInfoCliente();
-        System.out.println(estadosComInfoCliente);
-        return new Estados();
+    public EstadosComInfoCliente getEstados() {
+        return repository.getEstadosComInfoCliente();
     }
 }
